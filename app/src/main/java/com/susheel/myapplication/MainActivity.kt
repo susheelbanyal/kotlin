bc652db39c3bce511f1a3c32fcd3e7a4311f1f64
banyalsusheel <banyalsusheel@gmail.com>
2019-01-18 21:11:29 +0530

    Toast click

---
+++ app/src/main/java/com/susheel/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.susheel.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnShowToast.setOnClickListener {
+            Log.i( "MainActivity", "button clicked !")
+            Toast.makeText(this, "Button was clicked", Toast.LENGTH_SHORT).show()        }
+    }
+}
